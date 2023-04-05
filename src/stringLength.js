974944f7ce81c5e39f55a802c082c60a07e1912f
Joy Phoebe <joyapisi@gmail.com>
2023-04-05 13:42:45 +0300

    add string length testwith conditiona;s

--- src/stringLength.js
+++ src/stringLength.js
@@ -1,4 +1,10 @@
 function stringLength(string) {
+  if (string.length < 1) {
+    throw new Error('String must be at least 1 character long');
+  }
+  if (string.length > 10) {
+    throw new Error('String cannot be longer than 10 characters');
+  }
   return string.length;
 }
 
